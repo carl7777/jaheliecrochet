a83095b3c8efc51f4f39006dbf4cfb58cd9ef6a4
carl7777 <57014088+carl7777@users.noreply.github.com>
2022-09-23 03:32:48 -0300

    Add files via upload

---
+++ src/data.js
@@ -0,0 +1,89 @@
+export default {
+    products: [
+        {
+            _id: '1',
+            name: 'Description 1',
+            category: 'Shirts',
+            image: 'images/product-1.jpeg',
+            price: 60,
+            brand: 'Marque/Model',
+            rating: 4.5,
+            numReviews: 10,
+            countInStock: 6
+        },
+
+        {
+            _id: '2',
+            name: 'Description 2',
+            category: 'Category 1',
+            image: 'images/product-2.jpeg',
+            price: 40,
+            brand: 'Marque/Model',
+            rating: 4.5,
+            numReviews: 10,
+            countInStock: 6
+        },
+
+        {
+            _id: '3',
+            name: 'Description 3',
+            category: 'Shirts',
+            image: 'images/product-3.jpeg',
+            price: 30,
+            brand: 'Marque/Model',
+            rating: 4.5,
+            numReviews: 10,
+            countInStock: 6
+        },
+
+        {
+            _id: '4',
+            name: 'Description 4',
+            category: 'Shirts',
+            image: 'images/product-4.jpeg',
+            price: 20,
+            brand: 'Marque/Model',
+            rating: 4.5,
+            numReviews: 10,
+            countInStock: 6
+        },
+
+        {
+            _id: '5',
+            name: 'Description 5',
+            category: 'Shirts',
+            image: 'images/product-5.jpeg',
+            price: 70,
+            brand: 'Marque/Model',
+            rating: 4.5,
+            numReviews: 10,
+            countInStock: 6
+        },
+
+        {
+            _id: '6',
+            name: 'Description 6',
+            category: 'Shirts',
+            image: 'images/product-6.jpeg',
+            price: 55,
+            brand: 'Marque/Model',
+            rating: 4.5,
+            numReviews: 10,
+            countInStock: 6
+        },
+
+        {
+            _id: '7',
+            name: 'Description 7',
+            category: 'Shirts',
+            image: 'images/product-7.jpeg',
+            price: 35,
+            brand: 'Marque/Model',
+            rating: 4.5,
+            numReviews: 10,
+            countInStock: 6
+        },
+
+    ],
+};
+
